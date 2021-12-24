c18bfd97e90e270def57ed8355df08c03c9e3cf2
THE FALCON <57741232+FLCN-16@users.noreply.github.com>
2021-12-24 16:48:38 +0530

    Added production optimised code for prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,16 @@
 import bcrypt from 'bcrypt';
 import { PrismaClient, Prisma } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 /************************ Middlewares **********************/
 
