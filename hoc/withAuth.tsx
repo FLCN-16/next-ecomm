51b18ed5367606f7b87a5fe79f0837d6cb6796a8
THE FALCON <57741232+FLCN-16@users.noreply.github.com>
2021-12-29 15:32:20 +0530

    Added state in Auth HOC

--- hoc/withAuth.tsx
+++ hoc/withAuth.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux'
 import LoadingComponent from '../containers/Backend/Loading'
 
 
@@ -9,15 +10,16 @@ function withAuth(WrappedComponent: React.ComponentType, capabilities: string | 
   const AuthComponent = (props: any) => {
     const [auth, setAuth] = useState<AuthType>(null)
     const router = useRouter()
+    const isAuthenticated = useSelector(state => state.auth.get('isAuthenticated'));
 
     useEffect( () => {
       if ( auth !== null ) return;
 
-      setAuth(true)
-    }, [auth])
+      setAuth(isAuthenticated)
+    }, [auth, isAuthenticated])
 
     if ( auth === false ) {
-      router.push('/admin/auth')
+      router.push( '/admin/auth?next=' + window.encodeURI( router.pathname ) )
     }
 
     return ( auth ? <WrappedComponent /> : <LoadingComponent /> );
